a9dab02202291f89cd8c7bd7d22b0ea0e90196ad
ElwinBeall <elwinbeall@gmail.com>
2026-09-15 15:13:11 -0500

    add tictac.h for #defines and functions

--- tictac.c
+++ tictac.c
@@ -3,91 +3,48 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-
-#define x 'X'   // X or cross
-#define o 'O'   // O or naught
-#define spc ' ' // empty space
-
-// Since game_board is on the stack, just use array notation
-void init_game_board(char game_board[][3])
-{
-    for (size_t row = 0; row < 3; row++)
-    {
-        for (size_t col = 0; col < 3; col++)
-        {
-            game_board[row][col] = spc;
-        }
-    }
-}
-
-// Again, using the stack so no need for pointers
-void print_game_board(char game_board[][3])
-{
-    printf("\n");
-    for (size_t row = 0; row < 3; row++)
-    {
-        printf(" %c | %c | %c \n", game_board[row][0], game_board[row][1], game_board[row][2]);
-        if (row < 2)
-            printf("---|---|---\n");
-    }
-    printf("\n");
-}
-
-void make_human_move(char game_board[][3])
-{
-    printf("Enter your move (1-9) ");
-    // fgets()
-}
+#include "tictac.h"
 
 int main(void)
 {
-    const uint8_t winning_lines[8][3] = {
-        {0, 1, 2},
-        {3, 4, 5},
-        {6, 7, 8},
-        {0, 3, 6},
-        {1, 4, 7},
-        {2, 5, 8},
-        {0, 4, 8},
-        {2, 4, 6}};
-    uint8_t move_queue[5] = {0};
+    bool quit_playing, win = false;
 
-    // knowledge_base of move priority
-    uint8_t knowledge_base[10] = {2, 6, 8, 4, 7, 3, 1, 9, 5, 2};
-    char game_board[3][3] = {0};
+    GameState tictac = init_game();
 
-    /*  game_state flags:    only bits 1-4 are used, 8 7 6 5 4 3 2 1
-        bit     value   meaning
-        1       1       0 = Still playing, 1 = Game Over
-        2       2       0 = Player vs Computer, 1 = Computer vs Computer
-        3       4       0 = A player won, 1 = It's a Draw (Invalid if bit 1 = 0)
-        4       8       0 = Player 1 wins, 1 = Player 2 wins (Invalid if bit 1 = 0 or bit 3 = 1)
-    */
-    uint8_t game_state = 0;
+    while (!quit_playing)
+    {
+        /* Machine Move */
+        make_computer_move(&tictac);
 
-    /*Remove when done*/ printf("\n%c=%d, %c=%d, '%c'=%d\n", x, x, o, o, spc, spc);
-    init_game_board(game_board);
-    print_game_board(game_board);
+        /* Print or Update Game Board */
+        print_game_board(tictac.board);
 
-    /*
-    /// Game Loop ///
-    */
+        /* Check for Win */
+        if (win = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
+        {
+            printf("\nwin=%d", win);
+            break;
+        }
+        printf("\nwin=%d", win);
 
-    /* Machine Move */
+        /* Human Move */
+        while (!get_human_move(*tictac.board))
+            print_game_board(tictac.board);
 
-    /* Print or Update Game Board */
+        /* Print or Update Game Board */
+        print_game_board(tictac.board);
 
-    /* Check for Win */
-
-    /* Human Move */
-
-    /* Print or Update Game Board */
-
-    /* Check for Win */
-
-    /*
-    /// End Game Loop ///
-    */
-
+        /* Check for Win */
+        if (win = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
+        {
+            printf("\nwin=%d", win);
+            break;
+        }
+        printf("\nwin=%d", win);
+    }
+    printf("\n");
+    for (uint8_t i = 0; i < 5; i++)
+        printf("%d, ", tictac.move_queue[i]);
+    printf("\n");
     return EXIT_SUCCESS;
 }
---
+++ tictac.h
@@ -0,0 +1,269 @@
+#pragma once
+
+#define ROWS 3
+#define COLS 3
+// define players and empty space
+#define X_VAL 'X'
+#define O_VAL 'O'
+#define SPC ' '
+// convert 1D array indexing to 2D matrix indexing and vice versa
+#define GET_INDEX(row, num_cols, col) ((row) * (num_cols) + (col))
+#define GET_ROW(index, num_cols) ((index) / (num_cols))
+#define GET_COL(index, num_cols) ((index) % (num_cols))
+
+typedef struct GameState
+{
+    const uint8_t winning_lines[8][COLS];
+    uint8_t knowledge_base[10];
+    uint8_t board[ROWS][COLS];
+    uint8_t move_queue[5];
+    uint8_t move_count;
+    uint8_t winner; // X_VAL, O_VAL, 1 for Draw
+    bool is_game_over;
+} GameState;
+
+typedef struct RowCol
+{
+    uint8_t row;
+    uint8_t col;
+} RowCol;
+
+// Initialize game
+GameState init_game(void)
+{
+    GameState game = {
+        .winning_lines = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}},
+        .knowledge_base = {1, 5, 7, 3, 6, 2, 0, 8, 4, 1},
+        .board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        .move_queue = {0, 0, 0, 0, 0},
+        .move_count = 0,
+        .winner = 0,
+        .is_game_over = false};
+    return game;
+}
+
+// Reset game
+void reset_game(struct GameState *game)
+{
+    for (uint8_t row = 0; row < 3; row++)
+    {
+        for (uint8_t col = 0; col < 3; col++)
+        {
+            game->board[row][col] = ' ';
+        }
+    }
+
+    for (uint8_t cell = 0; cell < 5; cell++)
+    {
+        game->move_queue[cell] = 0;
+    }
+
+    game->move_count = 0;
+    game->winner = 0;
+    game->is_game_over = false;
+}
+
+bool cell_available(uint8_t board[][COLS])
+{
+    for (uint8_t row = 0; row < 3; row++)
+    {
+        if (board[row][0] == ' ' ||
+            board[row][1] == ' ' ||
+            board[row][2] == ' ')
+            return true;
+    }
+    return false;
+}
+
+// Print game board
+void print_game_board(uint8_t board[][COLS])
+{
+    uint8_t board_map[][COLS] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    printf("\n");
+    for (uint8_t row = 0; row < 3; row++)
+    {
+        printf(" %c | %c | %c \t %c | %c | %c \n",
+               board[row][0], board[row][1], board[row][2],
+               board_map[row][0], board_map[row][1], board_map[row][2]);
+        if (row < 2)
+            printf("---|---|---\t---|---|---\n");
+    }
+    printf("\n");
+}
+
+/* Human Move */
+bool get_human_move(uint8_t *board)
+{
+    uint8_t discard, input = 0;
+
+    printf("Enter your move (1-9) ");
+    input = getchar();
+    while ((discard = getchar() != '\n' && discard != EOF))
+        ;
+
+    if (input < '1' || input > '9')
+        return false;
+    else if (board[input - 49] != SPC)
+    {
+        printf("%s occupied location %c.\n",
+               board[input - 49] == O_VAL ? "You have already" : "Player X has already",
+               input);
+        return false;
+    }
+    else
+    {
+        board[input - 49] = O_VAL; // subtract ascii value of '1'
+        return true;
+    }
+}
+
+/* Computer Move */
+bool make_computer_move(struct GameState *game)
+{
+    // Winning/Blocking move
+    for (uint8_t row = 0; row < 8; row++)
+    {
+        uint8_t A_x, A_y, B_x, B_y, C_x, C_y; // 2D row/col
+        A_x = GET_ROW(game->winning_lines[row][0], COLS);
+        A_y = GET_COL(game->winning_lines[row][0], COLS);
+
+        B_x = GET_ROW(game->winning_lines[row][1], COLS);
+        B_y = GET_COL(game->winning_lines[row][1], COLS);
+
+        C_x = GET_ROW(game->winning_lines[row][2], COLS);
+        C_y = GET_COL(game->winning_lines[row][2], COLS);
+
+        if (game->board[A_x][A_y] ==
+                game->board[B_x][B_y] &&
+            game->board[C_x][C_y] == SPC &&
+            game->board[A_x][A_y] != SPC)
+        {
+            game->board[C_x][C_y] = X_VAL;
+            game->move_queue[game->move_count] = GET_INDEX(C_x, COLS, C_y);
+            game->move_count++;
+            // remove printf()
+            printf("WB-C row=%d, col=%d\n", C_x, C_y);
+            printf("WB ind=%d", game->move_queue[game->move_count - 1]);
+            if (cell_available(game->board))
+                return game->is_game_over;
+            else
+            {
+                printf("Draw\n");
+                game->winner = 1;
+                return game->is_game_over = true;
+            }
+        }
+        else if (
+            game->board[A_x][A_y] ==
+                game->board[C_x][C_y] &&
+            game->board[B_x][B_y] == SPC &&
+            game->board[A_x][A_y] != SPC)
+        {
+            game->board[B_x][B_y] = X_VAL;
+            game->move_queue[game->move_count] = GET_INDEX(B_x, COLS, B_y);
+            game->move_count++;
+            // remove printf()
+            printf("WB-B row=%d, col=%d\n", B_x, B_y);
+            printf("WB ind=%d", game->move_queue[game->move_count - 1]);
+            if (cell_available(game->board))
+                return game->is_game_over;
+            else
+            {
+                printf("Draw\n");
+                game->winner = 1;
+                return game->is_game_over = true;
+            }
+        }
+        else if (
+            game->board[B_x][B_y] ==
+                game->board[C_x][C_y] &&
+            game->board[A_x][A_y] == SPC &&
+            game->board[B_x][B_y] != SPC)
+        {
+            game->board[A_x][A_y] = X_VAL;
+            game->move_queue[game->move_count] = GET_INDEX(A_x, COLS, A_y);
+            game->move_count++;
+            // remove printf()
+            printf("WB-A row=%d, col=%d\n", A_x, A_y);
+            printf("WB ind=%d", game->move_queue[game->move_count - 1]);
+            if (cell_available(game->board))
+                return game->is_game_over;
+            else
+            {
+                printf("Draw\n");
+                game->winner = 1;
+                return game->is_game_over = true;
+            }
+        }
+    }
+
+    // Knowledge Base move
+    for (uint8_t cell = 0; cell < 10; cell++)
+    {
+        uint8_t row = GET_ROW(game->knowledge_base[cell], COLS);
+        uint8_t col = GET_COL(game->knowledge_base[cell], COLS);
+
+        if (game->board[row][col] == SPC)
+        {
+            game->board[row][col] = X_VAL;
+            game->move_queue[game->move_count] = GET_INDEX(row, COLS, col);
+            game->move_count++;
+            // remove printf()
+            printf("KB ind=%d", game->move_queue[game->move_count - 1]);
+
+            if (cell_available(game->board))
+                return game->is_game_over;
+            else
+            {
+                game->winner = 1;
+                return game->is_game_over = true;
+            }
+        }
+    }
+
+    // Random move?
+
+    // Fail to a draw
+    game->winner = 1;
+    return game->is_game_over = true;
+}
+
+/* Check for Win */
+bool check_win(const uint8_t winning_lines[][COLS], uint8_t board[][COLS], uint8_t *winner, bool *is_game_over)
+{
+    for (uint8_t row = 0; row < 8; row++)
+    {
+        uint8_t A_x, A_y, B_x, B_y, C_x, C_y; // 2D row/col
+        A_x = GET_ROW(winning_lines[row][0], COLS);
+        A_y = GET_COL(winning_lines[row][0], COLS);
+
+        B_x = GET_ROW(winning_lines[row][1], COLS);
+        B_y = GET_COL(winning_lines[row][1], COLS);
+
+        C_x = GET_ROW(winning_lines[row][2], COLS);
+        C_y = GET_COL(winning_lines[row][2], COLS);
+
+        if (board[A_x][A_y] == SPC)
+        {
+            continue;
+        }
+
+        if (board[A_x][A_y] ==
+                board[B_x][B_y] &&
+            board[A_x][A_y] ==
+                board[C_x][C_y])
+        {
+            *winner = (board[A_x][A_y] == X_VAL) ? X_VAL : O_VAL;
+            return *is_game_over = true;
+        }
+    }
+    return *is_game_over = false;
+}
