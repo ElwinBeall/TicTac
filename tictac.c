6261ec38b3e7d635caebcbde15f90e5ee08a4e11
ElwinBeall <elwinbeall@gmail.com>
2026-09-16 21:38:05 -0500

    added win and draw logic to tictac.c

--- tictac.c
+++ tictac.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-    bool quit_playing, win = false;
+    bool quit_playing, game_over = false;
 
     GameState tictac = init_game();
 
@@ -20,12 +20,15 @@ int main(void)
         print_game_board(tictac.board);
 
         /* Check for Win */
-        if (win = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
+        if (game_over = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
         {
-            printf("\nwin=%d", win);
+            printf("\nGame over. ", game_over);
+            if (tictac.winner == 1)
+                printf("It's a draw.\n");
+            else
+                printf("Player %c won.\n", tictac.winner == X_VAL ? X_VAL : O_VAL);
             break;
         }
-        printf("\nwin=%d", win);
 
         /* Human Move */
         while (!get_human_move(*tictac.board))
@@ -35,12 +38,15 @@ int main(void)
         print_game_board(tictac.board);
 
         /* Check for Win */
-        if (win = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
+        if (game_over = check_win(tictac.winning_lines, tictac.board, &tictac.winner, &tictac.is_game_over))
         {
-            printf("\nwin=%d", win);
+            printf("\nGame over. ", game_over);
+            if (tictac.winner == 1)
+                printf("It's a draw.\n");
+            else
+                printf("Player %c won.\n", tictac.winner == X_VAL ? X_VAL : O_VAL);
             break;
         }
-        printf("\nwin=%d", win);
     }
     printf("\n");
     for (uint8_t i = 0; i < 5; i++)
